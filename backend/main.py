5af5ffcfebbff90e3d79f8d46d3377a23f0fc5a1
aegonnvm-lab <aegon.nvm@gmail.com>
2026-08-19 21:32:19 +0530

    Update allowed origin for CORS configuration

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
-        "https://askmynotes-frontend-nkvo.onrender.com",
+        "https://ask-my-notes-2-z10v.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
